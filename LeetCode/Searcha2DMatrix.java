f3f44ddc6754874b172805d83be3d3375bb9c4ed
Kevin Manavalan <kevinseby20@gmail.com>
2024-04-15 13:30:10 +0530

    new leetcode prob

--- LeetCode/Searcha2DMatrix.java
+++ LeetCode/Searcha2DMatrix.java
@@ -1,4 +1,24 @@
 package LeetCode;
 
+//URL:https://leetcode.com/problems/search-a-2d-matrix/
 public class Searcha2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = (m * n) - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target > matrix[row][col]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
 }
